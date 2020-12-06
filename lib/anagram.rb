2d6f1c48afc83f098425714d91924a5b502fa739
Sam <sam@Sams-MacBook-Air.local>
2020-12-06 13:59:53 -0500

    commit tree d5e622e083ce877a104296222026aeb1fdab6028

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,22 @@
 # Your code goes here!
+class Anagram
+    attr_accessor :word
+
+    def initialize(word)
+        @word = word
+    end
+
+    def new_word
+        @word.split("").sort
+    end
+
+    def new_array(word)
+        word.split("").sort
+    end
+
+    def match(array)
+       array.select do |word|
+        new_array(word) == new_word
+       end
+    end
+end
